bce464aa1752ce33978e6686c2991e844ca281bc
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-09-08 10:51:47 +0530

    Tested out Microservice

--- src/main/scala/org/wdias/input/OnDemandInput.scala
+++ src/main/scala/org/wdias/input/OnDemandInput.scala
@@ -1,10 +1,10 @@
 import akka.actor.ActorSystem
-import akka.event.{LoggingAdapter, Logging}
+import akka.event.{Logging, LoggingAdapter}
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.client.RequestBuilding
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import akka.http.scaladsl.marshalling.ToResponseMarshallable
-import akka.http.scaladsl.model.{HttpResponse, HttpRequest}
+import akka.http.scaladsl.model.{HttpRequest, HttpResponse}
 import akka.http.scaladsl.model.StatusCodes._
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.unmarshalling.Unmarshal
@@ -13,41 +13,23 @@ import akka.stream.scaladsl.{Flow, Sink, Source}
 import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import java.io.IOException
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import scala.math._
 import spray.json.DefaultJsonProtocol
 
-case class IpInfo(query: String, country: Option[String], city: Option[String], lat: Option[Double], lon: Option[Double])
+case class Station(name: String, latitude: Double, longitude: Double)
 
-case class IpPairSummaryRequest(ip1: String, ip2: String)
+case class Unit(unit: String, `type`: String)
 
-case class IpPairSummary(distance: Option[Double], ip1Info: IpInfo, ip2Info: IpInfo)
+case class MetaData(station: Station, `type`: String, source: String, unit: Unit, tags: Array[String])
 
-object IpPairSummary {
-  def apply(ip1Info: IpInfo, ip2Info: IpInfo): IpPairSummary = IpPairSummary(calculateDistance(ip1Info, ip2Info), ip1Info, ip2Info)
-
-  private def calculateDistance(ip1Info: IpInfo, ip2Info: IpInfo): Option[Double] = {
-    (ip1Info.lat, ip1Info.lon, ip2Info.lat, ip2Info.lon) match {
-      case (Some(lat1), Some(lon1), Some(lat2), Some(lon2)) =>
-        // see http://www.movable-type.co.uk/scripts/latlong.html
-        val φ1 = toRadians(lat1)
-        val φ2 = toRadians(lat2)
-        val Δφ = toRadians(lat2 - lat1)
-        val Δλ = toRadians(lon2 - lon1)
-        val a = pow(sin(Δφ / 2), 2) + cos(φ1) * cos(φ2) * pow(sin(Δλ / 2), 2)
-        val c = 2 * atan2(sqrt(a), sqrt(1 - a))
-        Option(EarthRadius * c)
-      case _ => None
-    }
-  }
-
-  private val EarthRadius = 6371.0
-}
-
-trait Protocols extends DefaultJsonProtocol {
-  implicit val ipInfoFormat = jsonFormat5(IpInfo.apply)
-  implicit val ipPairSummaryRequestFormat = jsonFormat2(IpPairSummaryRequest.apply)
-  implicit val ipPairSummaryFormat = jsonFormat3(IpPairSummary.apply)
+trait Protocols extends SprayJsonSupport with DefaultJsonProtocol {
+  implicit val stationFormat = jsonFormat3(Station.apply)
+  implicit val unitFormat = jsonFormat2(Unit.apply)
+  implicit val metaDataFormat = jsonFormat5(MetaData.apply)
 }
 
 trait Service extends Protocols {
@@ -58,53 +40,23 @@ trait Service extends Protocols {
   def config: Config
   val logger: LoggingAdapter
 
-  lazy val ipApiConnectionFlow: Flow[HttpRequest, HttpResponse, Any] =
-    Http().outgoingConnection(config.getString("services.ip-api.host"), config.getInt("services.ip-api.port"))
-
-  def ipApiRequest(request: HttpRequest): Future[HttpResponse] = Source.single(request).via(ipApiConnectionFlow).runWith(Sink.head)
-
-  def fetchIpInfo(ip: String): Future[Either[String, IpInfo]] = {
-    ipApiRequest(RequestBuilding.Get(s"/json/$ip")).flatMap { response =>
-      response.status match {
-        case OK => Unmarshal(response.entity).to[IpInfo].map(Right(_))
-        case BadRequest => Future.successful(Left(s"$ip: incorrect IP format"))
-        case _ => Unmarshal(response.entity).to[String].flatMap { entity =>
-          val error = s"FreeGeoIP request failed with status code ${response.status} and entity $entity"
-          logger.error(error)
-          Future.failed(new IOException(error))
-        }
-      }
-    }
+  def storeObservedData(data: MetaData): Either[String, MetaData] = {
+    if(data.source == "WeatherStation") Right(data)
+    else Left("Source Not handling")
   }
 
   val routes = {
-    logRequestResult("akka-http-microservice") {
-      pathPrefix("ip") {
-        (get & path(Segment)) { ip =>
-          complete {
-            fetchIpInfo(ip).map[ToResponseMarshallable] {
-              case Right(ipInfo) => ipInfo
-              case Left(errorMessage) => BadRequest -> errorMessage
-            }
-          }
-        } ~
-          (post & entity(as[IpPairSummaryRequest])) { ipPairSummaryRequest =>
-            complete {
-              val ip1InfoFuture = fetchIpInfo(ipPairSummaryRequest.ip1)
-              val ip2InfoFuture = fetchIpInfo(ipPairSummaryRequest.ip2)
-              ip1InfoFuture.zip(ip2InfoFuture).map[ToResponseMarshallable] {
-                case (Right(info1), Right(info2)) => IpPairSummary(info1, info2)
-                case (Left(errorMessage), _) => BadRequest -> errorMessage
-                case (_, Left(errorMessage)) => BadRequest -> errorMessage
-              }
-            }
-          }
+    logRequestResult("on-demand-input") {
+      pathPrefix("observed") {
+        (post & entity(as[MetaData])) { observedData =>
+          complete(observedData)
+        }
       }
     }
   }
 }
 
-object AkkaHttpMicroservice extends App with Service {
+object OnDemandInput extends App with Service {
   override implicit val system = ActorSystem()
   override implicit val executor = system.dispatcher
   override implicit val materializer = ActorMaterializer()
