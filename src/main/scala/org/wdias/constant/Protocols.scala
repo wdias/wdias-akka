f4cf1ab55c28d40aef0a7d8d21069ac243fcfc84
Gihan Karunarathne <gckarunarathne@gmail.com>
2017-09-22 01:27:15 +0530

    Updated Data Transfering Protocols to handle TimeSeries

--- src/main/scala/org/wdias/constant/Protocols.scala
+++ src/main/scala/org/wdias/constant/Protocols.scala
@@ -9,8 +9,17 @@ case class Unit(unit: String, `type`: String)
 
 case class MetaData(station: Station, `type`: String, source: String, unit: Unit, tags: Array[String])
 
+case class Point(time: String, value: Double)
+
+case class TimeSeries(timeSeries: Array[Point])
+
+case class TimeSeriesEnvelop(metaData: MetaData, timeSeries: TimeSeries)
+
 trait Protocols extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val stationFormat = jsonFormat3(Station.apply)
   implicit val unitFormat = jsonFormat2(Unit.apply)
   implicit val metaDataFormat = jsonFormat5(MetaData.apply)
+  implicit val pointFormat = jsonFormat2(Point.apply)
+  implicit val timeSeriesFormat = jsonFormat1(TimeSeries.apply)
+  implicit val timeSeriesEnvelopFormat = jsonFormat2(TimeSeriesEnvelop.apply)
 }
