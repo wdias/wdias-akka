84d3226c1d2b8a8b77062f792bdde24e7b97a325
Gihan Karunarathne <gckarunarathne@gmail.com>
2018-03-16 06:26:00 +0530

    Fixed: Respond when Timeseries not found #6

--- src/main/scala/org/wdias/import/json/ImportJSON.scala
+++ src/main/scala/org/wdias/import/json/ImportJSON.scala
@@ -2,7 +2,6 @@ package org.wdias.`import`.json
 
 import akka.actor.{Actor, ActorIdentity, ActorLogging, ActorRef, Identify}
 import akka.http.scaladsl.model.StatusCodes.{NotFound}
-import akka.pattern.pipe
 import akka.util.Timeout
 import org.wdias.adapters.metadata_adapter.MetadataAdapter._
 import org.wdias.adapters.scalar_adapter.ScalarAdapter.{StoreTimeSeries, StoreTimeseriesResponse}
@@ -16,7 +15,9 @@ import scala.concurrent.duration._
 object ImportJSON {
 
   case class ImportJSONDataWithId(timeseriesId: String, data: List[DataPoint])
+
   case class ImportJSONDataWithMetadata(metadataObj: MetadataObj, data: List[DataPoint])
+
   case class ImportJSONDataWithMetadataIds(metadataIdsObj: MetadataIdsObj, data: List[DataPoint])
 
 }
@@ -25,7 +26,7 @@ class ImportJSON extends Actor with ActorLogging {
 
   import ImportJSON._
 
-  implicit val timeout: Timeout = Timeout(15 seconds)
+  implicit val timeout: Timeout = Timeout(20 seconds)
 
   var scalarAdapterRef: ActorRef = _
   context.actorSelection("/user/scalarAdapter") ! Identify(None)
@@ -36,13 +37,15 @@ class ImportJSON extends Actor with ActorLogging {
 
   def receive: Receive = {
     case ImportJSONDataWithId(timeseriesId: String, data: List[DataPoint]) =>
-      log.debug("Forwarding ImportJSONData With Id > ", scalarAdapterRef)
+      log.info("ImportJSONDataWithId With Id > {}", timeseriesId)
       val response: Future[Option[MetadataIdsObj]] = (metadataAdapterRef ? GetTimeseriesById(timeseriesId)).mapTo[Option[MetadataIdsObj]]
+      val ss = sender()
       response map { metadataIdsObj: Option[MetadataIdsObj] =>
-        if(metadataIdsObj.isDefined) {
+        log.info("Found Timeseries > {}", metadataIdsObj.getOrElse("None"))
+        if (metadataIdsObj.isDefined) {
           scalarAdapterRef forward StoreTimeSeries(TimeSeries(timeseriesId, metadataIdsObj.get, data))
         } else {
-          StoreTimeseriesResponse(NotFound, message = Option("Unable to find timeseries"))
+          ss ! StoreTimeseriesResponse(NotFound, message = Option("Unable to find timeseries"))
         }
       }
     case ImportJSONDataWithMetadata(metadataObj: MetadataObj, data: List[DataPoint]) =>
@@ -55,12 +58,13 @@ class ImportJSON extends Actor with ActorLogging {
         timeSeriesType = Option(metadataObj.timeSeriesType.toString),
         timeStepId = Option(metadataObj.timeStep.timeStepId)
       )).mapTo[Seq[MetadataIdsObj]]
-       response map { metadataIdsObjs: Seq[MetadataIdsObj] =>
-        if(metadataIdsObjs.nonEmpty) {
+      val ss = sender()
+      response map { metadataIdsObjs: Seq[MetadataIdsObj] =>
+        if (metadataIdsObjs.nonEmpty) {
           val metadataIdsObj = metadataIdsObjs.head
           scalarAdapterRef forward StoreTimeSeries(TimeSeries(metadataIdsObj.timeSeriesId, metadataIdsObj, data))
         } else {
-          StoreTimeseriesResponse(NotFound, message = Option("Unable to find timeseries"))
+          ss ! StoreTimeseriesResponse(NotFound, message = Option("Unable to find timeseries"))
         }
       }
     case ImportJSONDataWithMetadataIds(metadataIdsObj: MetadataIdsObj, data: List[DataPoint]) =>
@@ -73,12 +77,13 @@ class ImportJSON extends Actor with ActorLogging {
         timeSeriesType = Option(metadataIdsObj.timeSeriesType.toString),
         timeStepId = Option(metadataIdsObj.timeStepId)
       )).mapTo[Seq[MetadataIdsObj]]
+      val ss = sender()
       response map { metadataIdsObjs: Seq[MetadataIdsObj] =>
-        if(metadataIdsObjs.nonEmpty) {
+        if (metadataIdsObjs.nonEmpty) {
           val metadataIdsObj = metadataIdsObjs.head
           scalarAdapterRef forward StoreTimeSeries(TimeSeries(metadataIdsObj.timeSeriesId, metadataIdsObj, data))
         } else {
-          StoreTimeseriesResponse(NotFound, message = Option("Unable to find timeseries"))
+          ss ! StoreTimeseriesResponse(NotFound, message = Option("Unable to find timeseries"))
         }
       }
 
