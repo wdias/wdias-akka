2a12cb3ae24de5882babef0a0c8c08d6dc5df18a
Gihan Karunarathne <gckarunarathne@gmail.com>
2018-03-28 16:44:07 +0530

    Implemented Export JSON raw endpoints #6

--- src/main/scala/org/wdias/export/json/ExportJSONRoutes.scala
+++ src/main/scala/org/wdias/export/json/ExportJSONRoutes.scala
@@ -2,7 +2,6 @@ package org.wdias.export.json
 
 import akka.actor.{ActorRef, ActorSystem}
 import akka.event.Logging
-import akka.http.scaladsl.model.StatusCodes.Created
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
 import akka.http.scaladsl.server.directives.MethodDirectives.post
@@ -10,8 +9,9 @@ import akka.http.scaladsl.server.directives.RouteDirectives.complete
 import akka.pattern.ask
 import akka.stream.ActorMaterializer
 import akka.util.Timeout
-import org.wdias.constant.{Metadata, Protocols}
-import org.wdias.export.json.ExportJSON.ExportJSONData
+import org.wdias.adapters.scalar_adapter.ScalarAdapter.GetTimeseriesResponse
+import org.wdias.constant.{Metadata, MetadataIds, Protocols}
+import org.wdias.export.json.ExportJSON.{ExportJSONDataWithId, ExportJSONDataWithMetadata, ExportJSONDataWithMetadataIds}
 
 import scala.concurrent.Future
 import scala.concurrent.duration._
@@ -35,11 +35,45 @@ trait ExportJSONRoutes extends Protocols {
   // --- All Input Routes ---
   lazy val exportJSONRoutes: Route = {
     concat(
-      path("export" / "json" / "raw") {
-        (post & entity(as[Metadata])) { metaData: Metadata =>
-          logExportJSONRoutes.info("/export GET request: > {}", metaData)
-            complete(Created)
-        }
+      pathPrefix("export" / "json" / "raw") {
+        concat(
+          // GET: Get Timeseries with Id
+          (get & pathPrefix(Segment)) { timeseriesId: String =>
+            logExportJSONRoutes.info("/export/json/raw/XXX GET request: > {}, {}", timeseriesId)
+            val response: Future[GetTimeseriesResponse] = (exportJSONRef ? ExportJSONDataWithId(timeseriesId)).mapTo[GetTimeseriesResponse]
+            onSuccess(response) { result =>
+              if(result.timeseries.isDefined) {
+                complete(result.statusCode -> result.timeseries.get.data)
+              } else {
+                complete(result.statusCode -> result.message.get)
+              }
+            }
+          },
+          // POST: Get Timeseries with MetadataIds
+          (post & path("ids") & entity(as[MetadataIds])) { metadataIds: MetadataIds =>
+            logExportJSONRoutes.info("/export/json/raw/ids POST request: > {}", metadataIds)
+            val response: Future[GetTimeseriesResponse] = (exportJSONRef ? ExportJSONDataWithMetadataIds(metadataIds.toMetadataIdsObj)).mapTo[GetTimeseriesResponse]
+            onSuccess(response) { result =>
+              if(result.timeseries.isDefined) {
+                complete(result.statusCode -> result.timeseries.get.data)
+              } else {
+                complete(result.statusCode -> result.message.get)
+              }
+            }
+          },
+          // POST: Get Timeseries with Metadata
+          (post & entity(as[Metadata])) { metadata: Metadata =>
+            logExportJSONRoutes.info("/export/json/raw POST request: > {}", metadata)
+            val response: Future[GetTimeseriesResponse] = (exportJSONRef ? ExportJSONDataWithMetadata(metadata.toMetadataObj)).mapTo[GetTimeseriesResponse]
+            onSuccess(response) { result =>
+              if(result.timeseries.isDefined) {
+                complete(result.statusCode -> result.timeseries.get.data)
+              } else {
+                complete(result.statusCode -> result.message.get)
+              }
+            }
+          }
+        )
       }
     )
   }
